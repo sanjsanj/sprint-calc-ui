972d995d3e52742d47188fc2b8d1446166e8f7ff
Brian Hughes <brian.hughes@bglgroup.co.uk>
2018-01-31 16:09:10 +0000

    Add correct data

--- data.js
+++ data.js
@@ -1,14 +1,79 @@
 export const data = {
   "people" : [
     {
-      "name": "Bob",
-      "team": "Core",
-      "days": 1
+      "name": "Brian",
+      "team": "Sparkles",
+      "days": 0
     },
     {
-      "name": "Sue",
+      "name": "Sanjay",
       "team": "Sparkles",
-      "days": 2
+      "days": 0
+    },
+    {
+      "name": "Tom",
+      "team": "Sparkles",
+      "days": 0
+    },
+    {
+      "name": "Yago",
+      "team": "Sparkles",
+      "days": 0
+    },
+    {
+      "name": "Martin",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Stacey",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Ginevra",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Jeremy",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Dan",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Zoe",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Abi",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Hubert",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Freddie",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "James",
+      "team": "",
+      "days": 0
+    },
+    {
+      "name": "Andy",
+      "team": "Sparkles",
+      "days": 0
     }
   ]
 }
